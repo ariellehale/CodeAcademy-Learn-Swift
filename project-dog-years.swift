30e07744aa619b83987a2b36e6f3694b05abc315
Arielle Hale <arielle@ariellehale.com>
2019-12-16 10:55:58 -0800

    Calculate a dog's age in human years

---
+++ project-dog-years.swift
@@ -0,0 +1,17 @@
+// Gigi is 8 years old
+
+var dogAge: Double = 8
+
+var earlyYears: Double
+var laterYears: Double
+var humanYears: Double
+
+// First two years of a dog's life
+earlyYears = 21
+
+// Each of the follow years count as 4 human years
+laterYears = (dogAge - 2) * 4
+
+humanYears = earlyYears + laterYears
+
+print("My name is Gigi! Ruff ruff, I am \(humanYears) years old in human years.")
