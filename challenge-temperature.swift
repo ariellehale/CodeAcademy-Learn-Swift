525ef20700a45a405d385477a1edefe060503245
Arielle Hale <arielle@ariellehale.com>
2019-12-16 10:46:24 -0800

    Add files via upload

---
+++ challenge-temperature.swift
@@ -0,0 +1,6 @@
+// Convert the temperature to Celsius using the formula provided. Store the result in tempc.
+
+var tempf: Double = 80
+var tempc: Double = (tempf - 32) / 1.8
+
+print("The temp is \(tempc) degrees Celsius.")
