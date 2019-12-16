9dc94f90fc87bdec21291298c67ae19914b738ce
Arielle Hale <arielle@ariellehale.com>
2019-12-16 10:16:23 -0800

    Add files via upload

---
+++ project-block-letters.swift
@@ -0,0 +1,29 @@
+// My initials are S and L, so my Initials.swift program should output:
+print(" SSS   L")
+print("S   S  L")
+print("S      L")
+print(" SSS   L")
+print("    S  L")
+print("S   S  L")
+print(" SSS   LLLLL")
+
+// First, write a comment with a fun fact about yourself!
+// Fun fact: I am 30 years old!
+
+// Print your first initial.
+print("  A")
+print(" A A")
+print("A   A")
+print("AAAAA")
+print("A   A")
+print("A   A")
+print("A   A")
+
+// Print your second initial.
+print("H   H")
+print("H   H")
+print("H   H")
+print("HHHHH")
+print("H   H")
+print("H   H")
+print("H   H")
